08127704fcb95e199295ac6b39f9df4fcbb1756f
RimuruChan <179793046@qq.com>
2022-09-01 07:58:03 +0800

    [Feature] Use a better way to implement substring(str,str)

--- src/main/kotlin/com/liteldev/headeroutput/utils.kt
+++ src/main/kotlin/com/liteldev/headeroutput/utils.kt
@@ -8,21 +8,7 @@ fun String.relativePath(path: String) = Paths.get(this).relativize(Paths.get(pat
 fun String.parent() = "$this/.."
 
 fun String.substring(startStr: String = "", endStr: String = ""): String {
-    val start = if (startStr.isEmpty()) {
-        0
-    } else {
-        this.indexOf(startStr) + startStr.length
-    }
-    val str = this.substring(start)
-    val end = if (endStr.isEmpty()) {
-        str.length
-    } else {
-        str.indexOf(endStr)
-    }
-    if (end == -1) {
-        return ""
-    }
-    return str.substring(0, end)
+    return substringAfter(startStr, "").substringBefore(endStr, "")
 }
 
 fun StringBuilder.appendSpace(count: Int): StringBuilder {
