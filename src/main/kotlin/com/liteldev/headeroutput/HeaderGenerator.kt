4523c8ae83e4fb51e186723937ba46a453c636dd
RimuruChan <RealSprite233@outlook.com>
2023-07-18 20:36:42 +0800

    feat: add more stl header pre-include

--- src/main/kotlin/com/liteldev/headeroutput/HeaderGenerator.kt
+++ src/main/kotlin/com/liteldev/headeroutput/HeaderGenerator.kt
@@ -82,17 +82,49 @@ ${type.includeList.sorted().joinToString("\n") { "#include \"$it\"" }}
 
 #define MCAPI __declspec(dllimport)
 
-#include <forward_list>
-#include <list>
-#include <map>
-#include <memory>
-#include <optional>
-#include <set>
-#include <string>
-#include <string_view>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
+#include <algorithm>     // STL general algorithms
+#include <array>         // STL array container
+#include <bitset>        // STL bitset container
+#include <cctype>        // Character handling functions
+#include <cerrno>        // C Error codes
+#include <clocale>       // C localization library
+#include <cmath>         // Common mathematics functions
+#include <chrono>        // C++11 Time library
+#include <complex>       // Complex number type
+#include <cstdio>        // C Standard Input/Output library
+#include <cstdlib>       // General purpose utilities: program control, dynamic memory allocation, random numbers, sort and search
+#include <cstring>       // C string handling
+#include <ctime>         // C Time library
+#include <cwchar>        // Wide character type
+#include <cwctype>       // Wide character classification
+#include <deque>         // STL double ended queue container
+#include <exception>     // Exception handling classes
+#include <forward_list>  // STL forward list container
+#include <fstream>       // File stream classes
+#include <functional>    // STL Function objects
+#include <iomanip>       // Input/Output manipulators
+#include <ios>           // Base input/output stream classes
+#include <iosfwd>        // Input/Output forward declarations
+#include <iostream>      // Standard Input/Output stream objects
+#include <istream>       // Basic input stream classes
+#include <limits>        // Numeric limits
+#include <list>          // STL linear list container
+#include <map>           // STL map container
+#include <memory>        // STL unique_ptr, shared_ptr, weak_ptr
+#include <optional>      // STL optional type
+#include <ostream>       // Basic output stream classes
+#include <queue>         // STL queue and priority_queue container
+#include <set>           // STL set and multiset container
+#include <sstream>       // String stream classes
+#include <stack>         // STL stack container
+#include <stdexcept>     // Standard exception objects
+#include <streambuf>     // Stream buffer classes
+#include <string>        // String class
+#include <string_view>   // STL string_view type
+#include <unordered_map> // STL unordered map container
+#include <unordered_set> // STL unordered set container
+#include <utility>       // STL utility components
+#include <vector>        // STL dynamic array container
 
 """.trimIndent()
         )
