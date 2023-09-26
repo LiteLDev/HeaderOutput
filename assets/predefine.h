af7c49698a678483f53d3f7d2df0bd05dcd6daf1
OEOTYAN <1124718975@qq.com>
2023-09-27 00:47:25 +0800

    chore(config): update declare map

--- assets/predefine.h
+++ assets/predefine.h
@@ -104,7 +104,7 @@ template <typename T0, typename T1>
 class AutomaticID;
 class Dimension;
 using DimensionType = AutomaticID<Dimension, int>;
-// union GameRule::Value;
+namespace Bedrock {template <typename T, typename Err = std::error_code> class Result;}
 template <typename T0>
 class http_stl_allocator;
 using http_string  = std::basic_string<char, std::char_traits<char>, class http_stl_allocator<char>>;
